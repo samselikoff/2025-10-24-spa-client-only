064f6b6aa296b47b6954bcff7040cb9bbd7d3cb9
Sam Selikoff <sam.selikoff@gmail.com>
2025-10-24 17:14:26 -0400

    Add prefetch on hover

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -2,10 +2,11 @@
 
 import { Chat } from '@/lib/chats';
 import { domain } from '@/lib/domain';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import Link from 'next/link';
 
 export default function Sidebar() {
+  const queryClient = useQueryClient();
   const { data } = useQuery<Chat[]>({
     queryKey: ['chats'],
     queryFn: async () => {
@@ -26,6 +27,16 @@ export default function Sidebar() {
           className="px-4 py-2 hover:bg-gray-200"
           href={`/chat/${chat.id}`}
           key={chat.id}
+          onMouseEnter={() => {
+            queryClient.prefetchQuery({
+              queryKey: [`chat-${chat.id}`],
+              queryFn: async () => {
+                const response = await fetch(`${domain}/api/chats/${chat.id}`);
+
+                return await response.json();
+              },
+            });
+          }}
         >
           {chat.title}
         </Link>
